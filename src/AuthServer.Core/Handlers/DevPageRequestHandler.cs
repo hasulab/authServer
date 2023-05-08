76c078d338a794b793ee6465b8d87566f9edc404
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2023-05-08 22:10:19 +0100

    started mediator

---
+++ src/AuthServer.Core/Handlers/DevPageRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+
+namespace AuthServer.Handlers;
+
+public class DevPageRequestHandler : IRequestHandler<DevPageRequestHandler.Request, IResult>
+{
+    private readonly LinkGenerator _linker;
+    private readonly IAuthPageViewService _viewService;
+
+    public DevPageRequestHandler(LinkGenerator linker, IAuthPageViewService viewService)
+    {
+        _linker = linker;
+        _viewService = viewService;
+    }
+    public Task<IResult> Handle(Request request, CancellationToken cancellationToken)
+    {
+        var v2HomePage = _linker.GetPathByName(AuthPage.HomePageV2, values: new { tenantId = Guid.Empty });
+        return Task.FromResult(Results.Redirect(v2HomePage));
+    }
+
+    public class Request : IRequest<IResult>
+    {
+
+    }
+}
---
+++ src/AuthServer.Core/Handlers/HomePageRequestHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace AuthServer.Handlers;
+internal class HomePageRequestHandler : IRequestHandler<HomePageRequestHandler.Request, IResult>
+{
+    private readonly LinkGenerator _linker;
+    private readonly IAuthPageViewService _viewService;
+
+    public HomePageRequestHandler(LinkGenerator linker, IAuthPageViewService viewService)
+    {
+        _linker = linker;
+        _viewService = viewService;
+    }
+    public Task<IResult> Handle(Request request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(_viewService.RenderHomePage(request.TenantId));
+    }
+
+    internal record Request(string TenantId) : IRequest<IResult>;
+}
