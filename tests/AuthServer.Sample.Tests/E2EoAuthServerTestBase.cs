6eb927bc282878798a542c6980f84ab44e6b1b44
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2024-07-28 20:17:50 +0100

    refactored E2E tests

---
+++ tests/AuthServer.Sample.Tests/E2EoAuthServerTestBase.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthServer.Sample.Tests
+{
+    public class E2EoAuthServerTestBase
+    {
+        protected static string BuildAuthUrl(string url, Guid? tenantId = null)
+        {
+            tenantId = Guid.Empty;
+            return url.Replace("{tenantId}", tenantId.ToString());
+        }
+    }
+}
